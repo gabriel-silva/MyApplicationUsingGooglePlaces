a2603657a37ae6cc5c5eb6615a425d265af1a908
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-01-28 14:33:43 -0300

    adicionando navigation bottom view | menu/navigation | novos icones | nova cor

--- app/src/main/java/com/myapplicationusinggoogleplaces/MainActivity.kt
+++ app/src/main/java/com/myapplicationusinggoogleplaces/MainActivity.kt
@@ -2,11 +2,17 @@ package com.myapplicationusinggoogleplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.navigationView = findViewById(R.id.navigationView)
+
     }
 }
