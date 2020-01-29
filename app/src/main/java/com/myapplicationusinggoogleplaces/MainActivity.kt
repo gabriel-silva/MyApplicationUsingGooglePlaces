9a7766a48487dc45f86b180b62a0e89c02f1fdc0
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-01-29 01:04:27 -0300

    buscando dados da conta do facebook logada

--- app/src/main/java/com/myapplicationusinggoogleplaces/MainActivity.kt
+++ app/src/main/java/com/myapplicationusinggoogleplaces/MainActivity.kt
@@ -4,19 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.facebook.CallbackManager
-import com.facebook.FacebookSdk
+import com.facebook.*
 import com.facebook.appevents.AppEventsLogger
 import com.facebook.login.widget.LoginButton
 import java.util.*
-import com.facebook.FacebookException
 import com.facebook.login.LoginResult
-import com.facebook.FacebookCallback
+import org.json.JSONException
+import org.json.JSONObject
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     private var callbackManager: CallbackManager? = null
     private var loginButton: LoginButton? = null
+    private var accessTokenTracker: AccessTokenTracker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,28 +28,66 @@ class MainActivity : AppCompatActivity() {
 
         this.callbackManager = CallbackManager.Factory.create()
         this.loginButton = findViewById(R.id.login_button) as LoginButton
-        this.loginButton?.setReadPermissions(Arrays.asList("public_profile"))
+        this.loginButton?.setReadPermissions(Arrays.asList("email", "public_profile"))
 
         // Callback registration
-        this.loginButton?.registerCallback(this.callbackManager, object : FacebookCallback<LoginResult>{
-            override fun onSuccess(result: LoginResult?) {
-                Log.d("onSucess", "" + result.toString())
+        this.loginButton?.registerCallback(
+            this.callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    Log.d("onSucess", "" + result.toString())
+                }
+
+                override fun onCancel() {
+                    Log.d("onCancel", "onCancel")
+                }
+
+                override fun onError(error: FacebookException?) {
+                    Log.d("onSucess", "" + error.toString())
+                }
+
+            })
+
+        this.accessTokenTracker = object : AccessTokenTracker(){
+            override fun onCurrentAccessTokenChanged(oldAccessToken: AccessToken?, currentAccessToken: AccessToken?) {
+                if(currentAccessToken != null) {
+                    loadUserProfile(currentAccessToken)
+                }
             }
 
-            override fun onCancel() {
-                Log.d("onCancel", "onCancel")
-            }
-
-            override fun onError(error: FacebookException?) {
-                Log.d("onSucess", "" + error.toString())
-            }
-
-        })
-
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         this.callbackManager?.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun loadUserProfile(newAccessToken: AccessToken) {
+        var request: GraphRequest = GraphRequest.newMeRequest(newAccessToken, object : GraphRequest.GraphJSONObjectCallback {
+            override fun onCompleted(jsonObject: JSONObject?, response: GraphResponse?) {
+
+                try {
+                    var firstName: String? = jsonObject?.getString("first_name")
+                    var lastName: String? = jsonObject?.getString("last_name")
+                    var id: String? = jsonObject?.getString("id")
+                    val imageUrl = "https://graph.facebook.com/$id/picture?type=normal"
+
+                    Log.e("onCompleted: ", "firstName: $firstName lastName: $lastName id: $id")
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+
+
+            }
+
+        })
+
+        var parameters: Bundle = Bundle()
+        parameters.putString("fields", "first_name,last_name,id")
+        request.setParameters(parameters)
+        request.executeAsync()
+
     }
 }
