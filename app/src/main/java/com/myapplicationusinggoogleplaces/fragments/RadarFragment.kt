87e65e8605a22a939190b6415a63f13365ca3104
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-02-01 00:41:18 -0300

    adicionando Google Maps | Location

--- app/src/main/java/com/myapplicationusinggoogleplaces/fragments/RadarFragment.kt
+++ app/src/main/java/com/myapplicationusinggoogleplaces/fragments/RadarFragment.kt
@@ -1,22 +1,135 @@
 package com.myapplicationusinggoogleplaces.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.facebook.FacebookSdk.getApplicationContext
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.myapplicationusinggoogleplaces.R
 
-class RadarFragment : Fragment() {
+class RadarFragment : Fragment(), OnMapReadyCallback {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.radar_fragment, container, false)
-    }
+    private lateinit var mMap: GoogleMap
+    private var mLocationPermissionsGranted = false
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null
+
+    private val FINE_LOCATION: String = Manifest.permission.ACCESS_FINE_LOCATION
+    private val COURSE_LOCATION: String = Manifest.permission.ACCESS_COARSE_LOCATION
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1234
+    private val DEFAULT_ZOOM = 15f
 
     companion object {
         fun newInstance(): RadarFragment {
             return RadarFragment()
         }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.radar_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getLocationPermission()
+    }
+
+    private fun getDeviceLocation() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getApplicationContext())
+        try {
+            if (mLocationPermissionsGranted) {
+                val location: Task<Location>? = mFusedLocationProviderClient?.lastLocation
+                location?.addOnCompleteListener(object : OnCompleteListener<Location> {
+                    override fun onComplete(task: Task<Location>) {
+
+                        if (task.isSuccessful) {
+                            val currentLocation: Location = task.result as Location
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(currentLocation.latitude, currentLocation.longitude), DEFAULT_ZOOM))
+                        } else {
+                            Toast.makeText(getApplicationContext(), "unable to get current location", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+
+                })
+            }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun initMap() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun getLocationPermission() {
+        val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionsGranted = true
+                initMap()
+            } else {
+                requestPermissions(permissions, LOCATION_PERMISSION_REQUEST_CODE)
+            }
+        } else {
+            requestPermissions(permissions, LOCATION_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
+        mLocationPermissionsGranted = false
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty()) {
+                    var i = 0
+                    while (i < grantResults.size) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionsGranted = false
+                            return
+                        }
+                        i++
+                    }
+                    mLocationPermissionsGranted = true
+                    initMap()
+                }
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        if (mLocationPermissionsGranted) {
+            getDeviceLocation()
+
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return
+            }
+
+            mMap.isMyLocationEnabled = true
+            mMap.uiSettings.isMyLocationButtonEnabled = true
+            mMap.uiSettings.isZoomControlsEnabled = true
+        }
+
     }
 
 }
